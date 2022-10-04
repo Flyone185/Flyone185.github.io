edf5f3a1377a6efe2128740fd930fe4bf9a4d8ed
Flyone185 <urielmoreno185@gmail.com>
2022-10-04 14:02:16 -0300

    actualizacion index
    se agregan estilos mas lindos y un popup card al seleccionar los iconos que solo funciona en pantallas de escritorio y aun no en celulares

---
+++ Js/script.js
@@ -0,0 +1,38 @@
+//MUESTRA EL MODAL CUANDO SE SELECCIONA EL ICONO CORRESPONDIENTE
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(i => {
+  i.addEventListener('click', () => {
+    const modal = document.querySelector(i.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
