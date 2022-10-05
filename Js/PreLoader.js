691b347ae9c33c705357a16a73dfb1470d942180
Flyone185 <urielmoreno185@gmail.com>
2022-10-05 10:12:24 -0300

    fix

--- Js/PreLoader.js
+++ Js/PreLoader.js
@@ -1,6 +1,5 @@
 var loader = document.getElementById("preloader");
 var page = document.getElementById("page")
-
 window.addEventListener("load", function(){
   loader.style.display = "none";
   page.style.display = "initial";
