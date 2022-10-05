07cafc6868de1343c726a60bf0b30e7bb0333bfa
Flyone185 <urielmoreno185@gmail.com>
2022-10-05 09:38:24 -0300

    varios cambios
    se agrego preloader al repo aio, y se organizaron bien los estilos css y el js del preloader para que tengan su archivos independientes

---
+++ Js/PreLoader.js
@@ -0,0 +1,8 @@
+var loader = document.getElementById("preloader");
+var page = document.getElementById("page")
+
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+  page.style.display = "initial";
+  
+})
