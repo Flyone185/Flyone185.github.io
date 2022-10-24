34b61e86823c6e8859546a23cbfed1116555e4b4
Flyone185 <urielmoreno185@gmail.com>
2022-10-24 14:57:58 -0300

    creacion tickets page
    se crea la agina de tickets para la compra de tickets

---
+++ Websites/Parctica Integradora Bootstrap/Javascript/CalculoTotalTickets.js
@@ -0,0 +1,11 @@
+let select = document.getElementById("categoria");
+
+function calculaTotal(){
+    switch(select.selectedIndex){
+        case 0:
+
+        break;
+    }
+}
+//en este js tengo que ver de calcular el total de las entradas dependiendo de la categoria
+//tambien hay que programar el boton borrar
